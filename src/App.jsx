8ec19d0e7e9711d70f4b182a6b1f392dd6382d08
Celia Garrido <celiagarridoherrera@gmail.com>
2025-01-26 21:55:12 +0100

    finish app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import '../src/App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function ColorPicker() {
+  const [color, setColor] = useState("#FFFFFF");
+
+  function handleColorChange(event){
+    setColor(event.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='color-picker-conntainer'>
+      <h1>Color Picker</h1>
+      <div className='color-selected' style={{ backgroundColor: color }}>
+        <h2>Selected Color: {color}</h2>
+        </div>
+      <label>Select a Color: </label>
+      <br/>
+      <input type="color" value={color} onChange={handleColorChange} />
+    </div>
+  );
 }
 
-export default App
+export default ColorPicker;
